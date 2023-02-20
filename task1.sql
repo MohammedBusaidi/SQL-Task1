57811fc7b6e77d360d45efe02a906e2ff4dacfaf
MoodYBusaidi <moody.busaidi99@gmail.com>
2023-02-20 03:45:02 -0800

    added aggregation methods

--- task1.sql
+++ task1.sql
@@ -35,8 +35,6 @@ WHERE GPA > 3.5;
 DELETE FROM students WHERE Name like '%david%';
 DELETE FROM students WHERE ID = 2;
 
-
-
 create TABLE books (
 ID int IDENTITY(1,1) PRIMARY KEY,
 title text, 
@@ -124,3 +122,26 @@ INSERT INTO inventory VALUES (1,'iPhone 13','Apple Inc','Electronics',10,999.99)
 SELECT * FROM inventory;
 SELECT product_id , product_name , price FROM inventory where quantity > =10 ;
 SELECT * FROM inventory where price = 5 ;
+
+
+CREATE TABLE sales (
+  sale_id INT PRIMARY KEY,
+  sale_date DATE,
+  sale_amount DECIMAL(10,2)
+);
+
+INSERT INTO sales ( sale_id,sale_date, sale_amount)
+VALUES
+  (1,'2022-01-01', 100.00),
+  (2,'2022-01-02', 50.00),
+  (3,'2022-01-15', 75.00),
+  (4,'2022-02-01', 200.00),
+  (5,'2022-02-05', 150.00),
+  (6,'2022-02-20', 175.00);
+SELECT * FROM sales;
+SELECT month(sale_date) AS month, SUM(sale_amount) AS total_sales FROM sales GROUP BY MONTH(sale_date) ORDER BY MONTH(sale_date);
+SELECT AVG(sale_amount) AS avg_sales FROM sales;
+SELECT COUNT (sale_id) AS "Products sold" FROM sales;
+SELECT MIN(sale_amount) AS "lowest amount", MAX(sale_amount) AS "highest amount" FROM sales;
+SELECT * FROM sales ORDER BY sale_id DESC;
+SELECT * FROM sales WHERE sale_date BETWEEN '2022-01-01' AND '2022-02-01';
